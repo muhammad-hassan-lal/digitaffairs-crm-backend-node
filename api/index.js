4855ce558c4fdb96b1e3626d0d86f9d22f7c26b8
Muhammad Hassan Lal <muhammadhassanlal101@gmail.com>
2026-06-01 23:22:08 +0500

    change again

--- api/index.js
+++ api/index.js
@@ -2,37 +2,37 @@
 
 
 
-const app = require("../src/app");
-const sequelize = require("../src/config/db");
-
-let isConnected = false;
-
-module.exports = async (req, res) => {
-  try {
-    if (req.url === "/api/health" || req.url === "/health") {
-      return app(req, res);
-    }
-
-    if (!isConnected) {
-      await sequelize.authenticate();
-      console.log("Database connected");
-      isConnected = true;
-    }
-
-    return app(req, res);
-  } catch (error) {
-    console.error("API startup failed:", error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Database connection failed",
-      error: error.message,
-    });
-  }
-};
-
-
 // const app = require("../src/app");
+// const sequelize = require("../src/config/db");
 
-// module.exports = app;
+// let isConnected = false;
+
+// module.exports = async (req, res) => {
+//   try {
+//     if (req.url === "/api/health" || req.url === "/health") {
+//       return app(req, res);
+//     }
+
+//     if (!isConnected) {
+//       await sequelize.authenticate();
+//       console.log("Database connected");
+//       isConnected = true;
+//     }
+
+//     return app(req, res);
+//   } catch (error) {
+//     console.error("API startup failed:", error);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: "Database connection failed",
+//       error: error.message,
+//     });
+//   }
+// };
+
+
+const app = require("../src/app");
+
+module.exports = app;
 
